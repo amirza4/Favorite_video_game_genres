891b126871cbeb3efec37bccd1c9c043f381d0b0
akeemheshimu <akeem.heshimu@its.ny.gov>
2023-06-12 16:24:09 -0400

    Added a package labled stored_data. This package will contain all the released data that will be generated once the user goes to the next screen/activity.

---
+++ app/src/main/java/com/example/favorite_video_game_genres/stored_data/GenreRepo.kt
@@ -0,0 +1,8 @@
+package com.example.favorite_video_game_genres.stored_data
+
+import android.content.Context
+
+class GenreRepo(modePrivate: Int) {
+    val sharedPref = com.example.favorite_video_game_genres.stored_data.GenreRepo(Context.MODE_PRIVATE)
+
+}
