e076ce1d0670e8245b3c4fb23be22f5533dc43ee
akeemheshimu <akeem.heshimu@its.ny.gov>
2023-06-14 12:21:54 -0400

    Had to remove the Firebase.kt file. Firebase wasn't loading or connecting to the server when running the application. Failed to test application every time a change was made. Restarting and importing firestore and firebase back into the app underneath the MainActivity.kt. Merging in Ahsan's bargraph feature to test on.

--- app/src/main/java/com/example/favorite_video_game_genres/MainActivity.kt
+++ app/src/main/java/com/example/favorite_video_game_genres/MainActivity.kt
@@ -1,32 +1,37 @@
 package com.example.favorite_video_game_genres
 
 import android.os.Bundle
-import android.widget.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.*
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.*
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.*
-import androidx.compose.material3.*
-import androidx.compose.ui.draw.*
-import androidx.compose.ui.graphics.drawscope.*
-import androidx.compose.foundation.*
-import androidx.compose.ui.geometry.*
-import androidx.compose.ui.graphics.*
-
-import com.example.favorite_video_game_genres.ui.theme.Favorite_video_game_genresTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
+            // Access a Cloud Firestore instance from your Activity
+
+            val db = Firebase.firestore
             createScreen()
         }
     }
@@ -41,7 +46,7 @@ class MainActivity : ComponentActivity() {
         barChartTestData += Pair("Board Games", 7f)
         barChartTestData += Pair("Indie Games", 3f)
         barChartTestData += Pair("Racing Games", 8f)
-        barChartTestData += Pair("MMORPG's", 12f)
+        barChartTestData += Pair("MMORPG's",12f)
 
             Column(
             modifier = Modifier
