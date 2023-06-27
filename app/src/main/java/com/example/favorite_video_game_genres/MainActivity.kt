556416a796af3acd55ab76f6b8bd67a28201383d
akeemheshimu <akeem.heshimu@its.ny.gov>
2023-06-27 12:28:13 -0400

    Build was succesful for adding a switch button in. This is just a pre test before editing the Switch function to go back and forth between two UI modes (light & dark)

--- app/src/main/java/com/example/favorite_video_game_genres/MainActivity.kt
+++ app/src/main/java/com/example/favorite_video_game_genres/MainActivity.kt
@@ -1,46 +1,38 @@
 package com.example.favorite_video_game_genres
 
 import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.geometry.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.*
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.*
-import androidx.compose.material3.*
-import androidx.compose.ui.draw.*
-import androidx.compose.ui.graphics.drawscope.*
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.ui.geometry.*
-import androidx.compose.ui.graphics.*
-import androidx.compose.material3.Button
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.compose.*
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.google.firebase.firestore.FirebaseFirestore
 
 
@@ -353,5 +345,16 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+    }
+    @Composable
+    fun Switch(
+        checked: Boolean,
+        onCheckedChange: ((Boolean) -> Unit)?,
+        modifier: Modifier = Modifier,
+        thumbContent: (@Composable () -> Unit)? = null,
+        enabled: Boolean = true,
+        colors: SwitchColors = SwitchDefaults.colors(),
+        interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    ): Unit {
     }
 }
