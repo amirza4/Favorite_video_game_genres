6726b5238a7467f978684d9dc2f800ce161e4597
akeemheshimu <akeem.heshimu@its.ny.gov>
2023-07-05 16:58:19 -0400

    learned from Ahsan how to implement the other kt files into one file

--- app/src/main/java/com/example/favorite_video_game_genres/WebView.kt
+++ app/src/main/java/com/example/favorite_video_game_genres/WebView.kt
@@ -1,42 +1,23 @@
 package com.example.favorite_video_game_genres
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.viewinterop.AndroidView
 
 class WebView(activityContext: Any?) : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent()
-
         }
     }
 
-
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainContent(){
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("FVG | WebView How-To-Guide", color = Color.White) }) },
-        content = { WebViewPage() }
-    )
-}
-
-@Composable
-fun WebViewPage(){
+fun WebViewPage(mainActivity: MainActivity){
     //String aUrl contains the its URL
     val aUrl = "https://its.ny.gov/"
 
