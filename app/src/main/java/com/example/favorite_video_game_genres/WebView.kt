4b4557423111f4510edcb14923716bfda2ca4508
akeemheshimu <akeem.heshimu@its.ny.gov>
2023-07-07 13:25:01 -0400

    Remove non used imports for clean up

--- app/src/main/java/com/example/favorite_video_game_genres/WebView.kt
+++ app/src/main/java/com/example/favorite_video_game_genres/WebView.kt
@@ -1,26 +1,40 @@
 package com.example.favorite_video_game_genres
 
-import android.os.Bundle
+
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 
-class WebView {
+class WebView : Fragment() {
     @Composable
-    fun WebViewPage(mainActivity: MainActivity) {
+    fun WebViewPage(mainActivity: MainActivity, navController: NavController) {
         var webViewContent: WebView? = null
 
-        Button(onClick = {
-            webViewContent?.loadUrl("https://its.ny.gov/")
-        }) {
-            Text("Open Web View")
+        Column() {
+            Button(onClick = {
+                webViewContent?.loadUrl("https://its.ny.gov/")
+            },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .padding(start = 60.dp, end = 60.dp, bottom = 10.dp)
+            )
+
+            {
+                Text("Open Web View")
+            }
         }
 
         AndroidView(
