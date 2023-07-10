9d32a6a438e1fdf83739c60df27295a0cf0d28ca
akeemheshimu <akeem.heshimu@its.ny.gov>
2023-07-10 10:38:34 -0400

    removed the webViewContent that was set as null. webViewContent is not associated with the actual WebView instance that is created by AndroidView.

--- app/src/main/java/com/example/favorite_video_game_genres/WebView.kt
+++ app/src/main/java/com/example/favorite_video_game_genres/WebView.kt
@@ -2,36 +2,31 @@ package com.example.favorite_video_game_genres
 
 
 import android.annotation.SuppressLint
+import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-//import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.navigation.NavController
+import androidx.compose.ui.viewinterop.AndroidView
 
 
 @SuppressLint("SuspiciousIndentation")
 @Composable
-    fun WebViewPage(navController: NavController) {
-        var webViewContent: WebView? = null
-            webViewContent?.loadUrl("https://raw.githubusercontent.com/amirza4/Favorite_video_game_genres/Offline_Support/README.md")
-
-        AndroidView(
-            modifier = Modifier.fillMaxSize(),
-            factory = { context ->
-                WebView(context).apply {
-                    webViewContent = this
-                    webViewClient = WebViewClient()
-                    settings.javaScriptEnabled = true
-                }
-            },
+    fun webViewPage(modifier: Modifier = Modifier,  urlToRender: String = "https://raw.githubusercontent.com/amirza4/Favorite_video_game_genres/Offline_Support/README.md") {
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                loadUrl(urlToRender)
+            }
+        },
             update = {}
         )
     }
