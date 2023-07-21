d120c0d2646a078df6afa20c632a3e61b1a278b1
amirza4 <ahsan.mirza@its.ny.gov>
2023-07-21 14:31:36 -0400

    Update button permissions bug fixed
    Issue with doing dont allow camera permissions manually after allowing it and taking a picture, needed to add permission launcher functionality to the update button if it was set to not allow

--- app/src/main/java/com/example/favorite_video_game_genres/screens/ImageDisplay.kt
+++ app/src/main/java/com/example/favorite_video_game_genres/screens/ImageDisplay.kt
@@ -1,8 +1,14 @@
 package com.example.favorite_video_game_genres.screens
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import android.util.Log
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -11,6 +17,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -26,10 +33,13 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import com.example.favorite_video_game_genres.data.DataManipulation
+import kotlinx.coroutines.runBlocking
 
 class ImageDisplay {
     @SuppressLint("NotConstructor")
@@ -96,11 +106,30 @@ class ImageDisplay {
                 )
             }
         }
+
+        var permissionGranted by remember { mutableStateOf(false) }
+        var showDialog by remember { mutableStateOf(0) }
+        var askPermissions by remember { mutableStateOf(false) }
+
+        runBlocking {
+            permissionGranted = dataManip.getCameraPermission() ?: false // grab permission state
+        }
+
         Box(modifier = Modifier.fillMaxSize().padding(bottom = 0.dp), contentAlignment = Alignment.BottomCenter)
         {
             Button(onClick =
             {
-                navController.navigate("CameraScreen")
+                if (permissionGranted) {
+                    navController.navigate("CameraScreen")
+                    {
+                        popUpTo("AddImageScreen")
+                        {
+                            inclusive = true
+                        }
+                    }
+                } else {
+                    askPermissions = true // if permission wasnt set to allow
+                }
             },
                 modifier = Modifier
                     .fillMaxWidth(.7f)
@@ -127,6 +156,88 @@ class ImageDisplay {
             {
                 Text("Delete", style = TextStyle(fontSize = 14.sp), color = dataManip.textLDModeColor)
             }
+        }
+
+        val permissionLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(),
+            onResult = { isGranted ->
+                permissionGranted = isGranted // set result from permission popup
+                if(!permissionGranted)
+                {
+                    if(!ActivityCompat.shouldShowRequestPermissionRationale(dataManip.activity, Manifest.permission.CAMERA)) //if dont allow is clicked or popup refuses to show up anymore
+                    {
+                        showDialog = 2
+                    }
+                    else // if tapped outside or no option is clicked
+                    {
+                        showDialog = 1
+                    }
+                }
+                else
+                {
+
+                    runBlocking { dataManip.updateCameraPermission(permissionGranted) }
+                    navController.navigate("CameraScreen") // if allowed
+                }
+                askPermissions = false
+            }
+        )
+
+        if(askPermissions)
+        {
+            permissionLauncher.launch(Manifest.permission.CAMERA) // then request it
+        }
+
+        if(showDialog == 1)
+        {
+            AlertDialog(
+                onDismissRequest = { },
+                containerColor = dataManip.bgColor,
+                title = { Text("Camera Permission Request", textAlign = TextAlign.Center, color = dataManip.textLDModeColor) },
+                text = { Text("Please grant permission to access your camera to continue.", fontSize = 18.sp, color = dataManip.textLDModeColor) }, //dialog for allowing
+                confirmButton = {
+                    Button(onClick = {
+                        askPermissions = true
+                        showDialog = 0
+                    }) {
+                        Text(text = "Allow")
+                    }
+                },
+                dismissButton = {
+                    Button(onClick = {
+                        showDialog = 0
+                        askPermissions = false
+                    }) {
+                        Text("Dismiss")
+                    }
+                }
+            )
+        }
+        else if(showDialog == 2)
+        {
+            AlertDialog(
+                onDismissRequest = { },
+                containerColor = dataManip.bgColor,
+                title = { Text("Access Denied.", textAlign = TextAlign.Center, color = dataManip.textLDModeColor) }, // dialog for sending to settings since dont allow is selected
+                text = { Text("You have set the permission to not allowed. If you would still like to continue, please click the go to settings button and adjust it manually.", fontSize = 18.sp, color = dataManip.textLDModeColor) },
+                confirmButton = {
+                    Button(onClick = {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        intent.data = Uri.fromParts("package", dataManip.context.packageName, null)
+                        dataManip.context.startActivity(intent)
+                        showDialog = 0
+                    }) {
+                        Text(text = "Go To Settings")
+                    }
+                },
+                dismissButton = {
+                    Button(onClick = {
+                        showDialog = 0
+                    }) {
+                        Text("Dismiss")
+                    }
+                }
+            )
         }
     }
 }
